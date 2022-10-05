4688a4a9d14e0353ab78d3877e28f3bfffed7dde
Tomas Joaquin Abraham <TomasAbraham9@hotmail.com>
2022-10-05 01:45:02 -0300

    avatar terminado

--- App_usuarios/urls.py
+++ App_usuarios/urls.py
@@ -34,7 +34,7 @@ urlpatterns = [
     path("form_crear_usuario/", crear_usuario, name="form_crear_usuario"),
     path('salir', LogoutView.as_view(next_page="ingresar/"), name='salir'),
     path("listar_usuarios/", listar_usuarios, name="listar_usuarios"),
-    path("editar_usuarios/<int:id>", editar_usuarios, name="form_editar_usuarios"),
+    path("editar_usuarios/<int:id>", editar_usuarios, name="editar_usuarios"),
     path("listar_usuarios/", listar_usuarios, name="listar_usuarios"),
     path("eliminar_usuario/<int:id>", eliminar_usuario, name="form_eliminar_usuario"),
     path("ver_perfil/", ver_perfil, name="ver_perfil"), 
