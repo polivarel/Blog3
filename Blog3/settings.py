484a6e94a2405c6a304fd53357481032fb88e962
polivarel <p.olivare@outlook.com>
2022-10-04 22:24:03 -0300

    Vista eventos

--- Blog3/settings.py
+++ Blog3/settings.py
@@ -158,5 +158,5 @@ STATICFILES_DIRS =[os.path.join(BASE_DIR,"static")]
 
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR,'media_cdn/')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'data/') # 'data' is my media folder
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/') # 'data' is my media folder
 MEDIA_URL = '/media/'
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Instalar chkeditor con pip
+pip install mysqlclient
