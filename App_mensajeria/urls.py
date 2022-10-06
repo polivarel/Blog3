4a162dbf3f571fbd91b59664acbf7adad6dcfc1e
polivarel <p.olivare@outlook.com>
2022-10-05 22:39:10 -0300

    Mensajes terminados, corregir colores

--- App_mensajeria/urls.py
+++ App_mensajeria/urls.py
@@ -6,6 +6,8 @@ from App_mensajeria.views import *
 
 
 urlpatterns = [
-    path("verContactos/"        , verContactos , name="verContactos"),
-    path("mensajeA/<int:id>", mensajeA    , name="mensajeA"   ),
+    path("verContactos/"           , verContactos , name="verContactos"),
+    path("mensajeA/<id>/<nick>"    , mensajeA     , name="Form_mensajeA"   ),
+    path("mensajes/<id1>/<id2>"    , mensajes     , name="Form_mensajes"   ),
+    path("mensajes_old/<id1>/<id2>/<nick>", mensajes_old , name="mensajes_old"    ),
 ]
